9a540c583c983fcac83d416c960dcf9ad313abf3
DietPancake <70340236+DietPancake@users.noreply.github.com>
2020-10-20 12:17:12 -0500

    Add files via upload

---
+++ Expirament Name/src/JLProject/JLChapter3Project.java
@@ -0,0 +1,30 @@
+package JLProject;
+import javax.swing.JOptionPane;
+
+public class JLChapter3Project {
+
+   
+    public static void main(String[] args) {
+    String fName = JOptionPane.showInputDialog("What is your first name?:");
+    String lName = JOptionPane.showInputDialog("What is you last name?:");
+    String Age = JOptionPane.showInputDialog("How old are you?:");
+    Integer.parseInt(Age);
+    int cYear = 2020;
+    int Year = (cYear - Integer.parseInt(Age));
+    System.out.println(fName);
+    System.out.println(lName);
+    System.out.println(Year);
+            };
+    static {
+        
+        
+              
+        
+        
+        
+    }
+    
+
+    
+    }
+
